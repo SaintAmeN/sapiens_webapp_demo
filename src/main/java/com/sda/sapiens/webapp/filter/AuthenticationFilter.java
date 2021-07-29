7f59dd57c477cb11a47abfaf61d176d0b2c30089
AmeN <pawel.reclaw@gmail.com>
2021-07-29 18:34:50 +0200

    Logger and filters, pierwsza częsc zajec, 29.07.2021.

---
+++ src/main/java/com/sda/sapiens/webapp/filter/AuthenticationFilter.java
@@ -0,0 +1,47 @@
+package com.sda.sapiens.webapp.filter;
+
+import com.sun.xml.bind.v2.runtime.reflect.opt.Const;
+import lombok.extern.slf4j.Slf4j;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+@Slf4j
+@WebFilter("/**")
+public class AuthenticationFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        String userNameValue = request.getHeader(FilterConstants.HEADER_AUTH_USER_NAME_KEY);
+        String userSurnameValue = request.getHeader(FilterConstants.HEADER_AUTH_USER_SURNAME_KEY);
+
+        if (userNameValue == null || userNameValue.isEmpty() ||
+                userSurnameValue == null || userSurnameValue.isEmpty()) {
+            log.info("Użytkownik nie jest autentykowany. Przekierowujemy na stronę logowania");
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+            // ścieżka domyślna = /webapp
+            response.sendRedirect(servletRequest.getServletContext().getContextPath() + "/login");
+            return;
+        }
+
+        // jeśli nie mamy powodu żeby zatrzymać request, przepuszczamy go dalej używając poniższej linii
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
