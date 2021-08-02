db935d313b0116d71ff9014cd849cbb1ccc465ed
AmeN <pawel.reclaw@gmail.com>
2021-08-02 21:14:24 +0200

    finis

--- src/main/java/com/sda/sapiens/webapp/dao/GradeDao.java
+++ src/main/java/com/sda/sapiens/webapp/dao/GradeDao.java
@@ -4,6 +4,8 @@ import com.sda.sapiens.webapp.model.Grade;
 
 import javax.ejb.Stateless;
 
+// deklarując stateless mówimy że obiekt ma mieć jedną instancję która ma się stworzyć
+// tuż po załadowaniu projektu i ma być dodana do DI
 @Stateless
 public class GradeDao implements EntityDao<Grade> {
 }
